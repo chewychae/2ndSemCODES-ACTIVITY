c9833f00f7efac9fd77a4762ab44d8df21098e69
Dubuuu <145317870+chewychae@users.noreply.github.com>
2024-03-06 11:37:30 +0800

    Add files via upload

---
+++ Rectangle/src/main/java/com/mycompany/rectangle/Rectangle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rectangle;
+
+/**
+ *
+ * @author Acer
+ */
+public class Rectangle {
+
+    public static void main(String[] args) {
+        rectangles area = new rectangles(3, 4);
+      double Area  = area.getArea();
+       System.out.println("The Area of the rectangle is: " + Area);
+    }
+}
---
+++ Rectangle/src/main/java/com/mycompany/rectangle/rectangles.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.rectangle;
+
+/**
+ *
+ * @author Acer
+ */
+public class rectangles {
+
+    double width;
+    double length;
+
+    public rectangles(double width, double length) {
+        this.width = width;
+        this.length = length;
+
+    }
+
+    double getArea() {
+        return width * length;
+    }
+}
