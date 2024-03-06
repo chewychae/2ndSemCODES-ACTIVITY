9bf47ff7b9779eaf4cc671eefaf79652800984cf
Dubuuu <145317870+chewychae@users.noreply.github.com>
2024-03-06 11:39:20 +0800

    Add files via upload

---
+++ Person/src/main/java/com/mycompany/person/Person.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.person;
+
+/**
+ *
+ * @author Acer
+ */
+public class Person {
+
+    public static void main(String[] args) {
+    self adult = new self("Glenn", 2);
+    adult.isAdult();
+    
+        System.out.println(adult);
+    
+    
+    
+    }
+}
